60d61787d95e4745045c8d5f3c25c94f0cb8e006
GearUser <105487991+GearUser@users.noreply.github.com>
2022-05-13 23:35:27 +0530

    Update config.py

--- bot/config.py
+++ bot/config.py
@@ -14,7 +14,7 @@ class Config(object):
     # dont u fucking remove this id 😤
     TG_BOT_TOKEN = "5302929820:AAEsKXElRmQ1vcwYwRYmQIYstnKPzLQF0-w"
     # the download location, where the HTTP Server runs
-    DOWNLOAD_LOCATION = "/downloads"
+    DOWNLOAD_LOCATION = "/downloader"
     # Telegram maximum file upload size
     BOT_USERNAME = "gear100bot"
     MAX_FILE_SIZE = 2097152000
