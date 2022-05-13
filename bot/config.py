eb6a71d48e5e9c6ca939003c058e61c2f7118f9e
GearUser <105487991+GearUser@users.noreply.github.com>
2022-05-13 23:08:15 +0530

    Update config.py

--- bot/config.py
+++ bot/config.py
@@ -1,41 +1,35 @@
-from bot.get_cfg import get_config
+
+import os
+
+
+
 class Config(object):
-    # You can keep this default
-    SESSION_NAME = get_config("SESSION_NAME", "AHCompressorBot")
-    # AHCompressBot....
-    # sucks Dude
-    APP_ID = int(get_config("APP_ID", 12345))
-    API_HASH = get_config("API_HASH", "")
-    LOG_CHANNEL = get_config("LOG_CHANNEL")
-    UPDATES_CHANNEL = get_config("UPDATES_CHANNEL", None) # Without `@` LOL
-     # Get these values from my.telegram.org
-    AUTH_USERS = set(
-        int(x) for x in get_config(
-            "AUTH_USERS",
-            should_prompt=True
-        ).split()
-    )
-# array , simplest method was AUTH_USERS = [] ; AUTH_USERS.append(your telegram id) 🤣
+    SESSION_NAME = "AHCompressorBot"
+    APP_ID = "3845818"
+    API_HASH = "95937bcf6bc0938f263fc7ad96959c6d"
+    LOG_CHANNEL = -1001763491746
+    UPDATES_CHANNEL = None # Without `@` LOL
+        AUTH_USERS = "1443454117"
     # array to store the channel ID who are authorized to use the bot
     # dont u fucking remove this id 😤
-    TG_BOT_TOKEN = get_config("TG_BOT_TOKEN", "")
+    TG_BOT_TOKEN = "5302929820:AAEsKXElRmQ1vcwYwRYmQIYstnKPzLQF0-w"
     # the download location, where the HTTP Server runs
-    DOWNLOAD_LOCATION = get_config("DOWNLOAD_LOCATION", "/app/downloads")
+    DOWNLOAD_LOCATION = "/downloads"
     # Telegram maximum file upload size
-    BOT_USERNAME = get_config("BOT_USERNAME", "")
+    BOT_USERNAME = "gear100bot"
     MAX_FILE_SIZE = 2097152000
     TG_MAX_FILE_SIZE = 2097152000
     FREE_USER_MAX_FILE_SIZE = 2097152000
     # default thumbnail to be used in the videos
-    DEF_THUMB_NAIL_VID_S = get_config("DEF_THUMB_NAIL_VID_S", "https://placehold.it/90x90")
+    DEF_THUMB_NAIL_VID_S = "https://placehold.it/90x90"
     # proxy for accessing youtube-dl in GeoRestricted Areas
     # Get your own proxy from https://github.com/rg3/youtube-dl/issues/1091#issuecomment-230163061
-    HTTP_PROXY = get_config("HTTP_PROXY", None)
+    HTTP_PROXY = None
     # maximum message length in Telegram
     MAX_MESSAGE_LENGTH = 4096
     # add config vars for the display progress
-    FINISHED_PROGRESS_STR = get_config("FINISHED_PROGRESS_STR", "▣")
-    UN_FINISHED_PROGRESS_STR = get_config("UN_FINISHED_PROGRESS_STR", "▢")
-    LOG_FILE_ZZGEVC = get_config("LOG_FILE_ZZGEVC", "Log.txt")
+    FINISHED_PROGRESS_STR = "▣"
+    UN_FINISHED_PROGRESS_STR = "▢"
+    LOG_FILE_ZZGEVC = "Log.txt"
       # because, https://t.me/c/1494623325/5603
-    SHOULD_USE_BUTTONS = get_config("SHOULD_USE_BUTTONS", False)
+    SHOULD_USE_BUTTONS = False
